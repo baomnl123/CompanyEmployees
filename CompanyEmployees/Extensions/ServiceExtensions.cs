b93e3219c0b76142be8f3696c1609f935d8d3b69
Long Bao <mglongbao@gmail.com>
2024-05-27 10:07:12 +0700

    2.4 - Configuring Logger Service for Logging Messages

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,3 +1,6 @@
+using Contracts;
+using LoggerService;
+
 namespace CompanyEmployees.Extensions;
 
 public static class ServiceExtensions
@@ -13,4 +16,7 @@ public static class ServiceExtensions
 
     public static void ConfigureIISIntegration(this IServiceCollection services) =>
         services.Configure<IISOptions>(options => { });
+
+    public static void ConfigureLoggerService(this IServiceCollection services) =>
+        services.AddSingleton<ILoggerManager, LoggerManager>();
 }
