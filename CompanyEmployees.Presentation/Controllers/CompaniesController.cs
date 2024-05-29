d0fa2834869f2130da816c03f77df0127bbd49bb
Long Bao <mglongbao@gmail.com>
2024-05-29 11:35:16 +0700

    4.1 - Controllers and Routing in WEB API

---
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Presentation.Controllers;
+
+[Route("api/[controllers]")]
+[ApiController]
+public class CompaniesController : ControllerBase { }
