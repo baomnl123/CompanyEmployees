7b51f7c608f19b58385d26012cdbf307b5cbdaa8
Long Bao <mglongbao@gmail.com>
2024-06-01 07:03:36 +0700

    7.3 - Testing Content Negotiation

--- src/CompanyEmployees/MappingProfile.cs
+++ src/CompanyEmployees/MappingProfile.cs
@@ -9,8 +9,8 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Company, CompanyDto>()
-            .ForCtorParam(
-                "FullAddress",
+            .ForMember(
+                c => c.FullAddress,
                 opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country))
             );
 
